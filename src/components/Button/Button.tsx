1f0c739cff5e40280476a09295c3ce02503f5ddd
mudziok <mudziok@gmail.com>
2022-05-18 00:40:11 +0200

    Create custom Button Component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const FontSizes = {
+  "sm": "0.8rem",
+  "md": "1.0rem",
+  "lg": "1.5rem",
+} as const;
+
+const ButtonSizes = {
+  "auto": "auto",
+  "full": "calc(100% - 1rem)"
+}
+
+type FontSize = keyof typeof FontSizes;
+type ButtonSize = keyof typeof ButtonSizes;
+
+const Button = styled("button")<{fontSize?: FontSize, buttonSize?: ButtonSize}>`
+  width: ${props => ButtonSizes[props.buttonSize || "auto"]};
+  font-size: ${props => FontSizes[props.fontSize || "md"]};
+  padding: ${props => FontSizes[props.fontSize || "md"]};
+  font-weight: bold;
+
+  border: none;
+  border-radius: ${props => props.theme.roundness};
+  background-color: ${props => props.theme.accent};
+  color: ${props => props.theme.background};
+  cursor: pointer;
+  margin: 0.5rem;
+  white-space: nowrap;
+
+  transition: background-color 0.05s ease-out;
+  &:hover {
+    background-color: ${props => props.theme.primary};
+  }
+`;
+
+export default Button;
